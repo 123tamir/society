f076cda812197d0ef4b0323da6e91e77819fd5cc
123tamir <139543953+123tamir@users.noreply.github.com>
2025-07-26 12:41:36 +0530

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
     password: document.getElementById('password').value
   };
 
-  const res = await fetch('http://localhost:5000/api/users/login', {
+  const res = await fetch('https://society-5hkq.onrender.com/api/users/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
